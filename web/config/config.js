bffaa64989f34bcf62ac328955a6ace434a3dfa1
jiangood <410518072@qq.com>
2026-01-29 11:22:59 +0800

    增加前端

---
+++ web/config/config.js
@@ -0,0 +1,8 @@
+import {defineConfig} from 'umi';
+import config from "@jiangood/admin-spring-boot-starter/config/dist/config";
+
+
+
+
+export default defineConfig(config);
+
---
+++ web/src/layouts/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Layouts} from "@jiangood/admin-spring-boot-starter";
+
+export default class extends React.Component {
+
+
+
+
+    render() {
+        return <Layouts {...this.props} ></Layouts>
+    }
+
+}
